13ec0e3a352c9a4c8ff4da37b8d16607735faff3
Marjon Ramos <ramosmarjonf@gmail.com>
2020-03-12 01:03:49 +0800

    6. Introducing migrations and models

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   <a href="/todos">TODOS</a>
                 </div>
 
                 <div class="links">
